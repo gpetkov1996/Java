fdf4a7f54b382e1d6ac63522edf6a5dcd1f568d8
Genadi Petkov <101999946+gpetkov1996@users.noreply.github.com>
2024-02-25 22:46:16 +0200

    Delete Udemy directory

--- Udemy/JavaToDoList/src/classes/ToDoList.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class ToDoList {
-
-    public static void main(String[] args) {
-        new AppFrame();
-    }
-}
--- Udemy/Udemy/Java 17 Masterclass Start Coding in 2024/BillsBurgers/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Meal meal = new Meal();
-        meal.buildAMeal();
-    }
-}
